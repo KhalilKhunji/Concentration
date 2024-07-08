80676a2f21e51f579789e46784c21962a1eb0ee0
KhalilKhunji <khalilkhunji@gmail.com>
2024-07-08 09:54:28 +0300

    Added variables/constants/cached elements and event listeners

--- js/app.js
+++ js/app.js
@@ -1,34 +1,43 @@
 // Constants
 // 1. Array of Cards
+const cards = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"];
 // 2. Number of Attempts
+const attempts = 6;
 // 3. Time Durations for Each Phase (Memorization and Matching)
-
+const memorization = 30;
+const matching = 60;
 
 // Variables
-// 1. Number of Cards on Display [Variable in initial according to stretch, but here according to number matched while the game is played.
+// 1. Number of Cards on Display [Variable in initial according to stretch, but here according to number matched while the game is played.]
+let displayCards = 10;
 // 2. Randomized Card Pairs from Array [Need Randomization function to ensure no overlapping pairs]
+let cardPairs;
 // 3. Cards chosen by player (Card A and Card B)
+let cardA = '';
+let cardB = '';
 // 4. Player's Score.
+let score = 0;
 // 5. Current Game Phase (Memorization or Match)
+let phase = 'memorization';
 // 6. Game Outcome/State (Win or Loss)
-
+let outcome = '';
 
 
 // Cached Elements
 // 1. The cards on display (For Each?), only rendered cards.
+const card = document.querySelectorAll(".card");
 // 2. Start/Reset Button
+const button = document.querySelector("#button");
 // 3. Timer(s)
-// 4. Score Update
-// 5. Attempt Update
-// 6. Message Box (Announces game start, matched pair outcomes, score changes, attempt changes, phase changes, time out, and game outcome).
-
-
-// 3. Timer
 const timerEl = document.querySelector("#timer");
+// 4. Score Update
+const scoreEl = document.querySelector("#score");
+// 5. Attempt Update
+const attemptEl = document.querySelector("#attempts");
+// 6. Message Box (Announces game start, matched pair outcomes, score changes, attempt changes, phase changes, time out, and game outcome).
+const messageEl = document.querySelector("message-box");
 
-// Event Listeners
-// 1. The Cards on display (For Each) to be clicked.
-// 2. Start/Reset Button
+
 
 
 // Functions
@@ -42,7 +51,7 @@ const timerEl = document.querySelector("#timer");
 
 // Timer Function:
 const timerFunction = () => {
-    let time = 30;
+    let time = memorization;
     const timer = setInterval(() => {
         time--;
         timerEl.innerHTML = time;
@@ -51,3 +60,10 @@ const timerFunction = () => {
         };
     }, 1000);
 };
+
+
+// Event Listeners
+// 1. The Cards on display (For Each) to be clicked.
+card.addEventListener("click",placeholder);
+// 2. Start/Reset Button
+button.addEventListener("click",init);
