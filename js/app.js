af5c2750ac3e7f2d46cf6ffb1d9cb307080d59f9
KhalilKhunji <khalilkhunji@gmail.com>
2024-07-10 13:03:06 +0300

    Removed pseudocode and cleaned code.

--- js/app.js
+++ js/app.js
@@ -1,52 +1,27 @@
-// Constants
-// 1. Card Pairs
 const pairs = [["dA","hA"],["sA","cA"],["dK","hK"],["sK","cK"],["dQ","hQ"],["sQ","cQ"],["dJ","hJ"],["sJ","cJ"],["d10","h10"],["s10","c10"],["d09","h09"],["s09","c09"],["d08","h08"],["s08","c08"],["d07","h07"],["s07","c07"],["d06","h06"],["s06","c06"],["d05","h05"],["s05","c05"],["d04","h04"],["s04","c04"],["d03","h03"],["s03","c03"],["d02","h02"],["s02","c02"]];
-// 2. Number of Attempts
-// 3. Time Durations for Each Phase (Memorization and Matching) [CHANGE TO 30 AFTER TESTING]
 const memorization = 10;
 const matching = 30;
 
-// Variables
-// 1. Number of Cards on Display [Variable in initial according to stretch, but here according to number matched while the game is played.]
 let displayCards = [];
-// 2. Number of matches:
 let match = 0;
-// 3. Cards chosen by player (Card A and Card B)
 let cardA = [];
 let cardB = [];
-// 4. Player's Score.
 let score = 0;
-// 5. Attempts Remaining:
 let attempts = 6; 
-// 7. Game State
 let state = false;
 
-// Cached Elements
-// 1. The cards on display, only rendered cards.
 const card = document.querySelectorAll(".card");
-// 2. Start/Reset Button
 const button = document.querySelector("#button");
-// 3. Timer(s)
 const timerEl = document.querySelector("#timer");
-// 4. Score Update
 const scoreEl = document.querySelector("#score");
-// 5. Attempt Update
 const attemptEl = document.querySelector("#attempts");
-// 6. Message Box (Announces game start, matched pair outcomes, score changes, attempt changes, phase changes, time out, and game outcome).
 const messageEl = document.querySelector("#message-box");
 
-
-
-// Functions
-
-// 1. Init function - Randomize Cards (no overlap) - Memorization Timer Start. [init should end with render()]
 const init = () => {
     if(state === false) {
         state = true;
-        timerEl.innerHTML = `<h2>Memorization Time Remaining: 10 seconds</h2>`;
-        scoreEl.innerHTML = '';
+        timerEl.innerHTML = '<h2>Memorization Time Remaining: 10 seconds</h2>';
         scoreEl.innerHTML = '<p>Player Score: 0/5</p>';
-        attemptEl.innerHTML = '';
         attemptEl.innerHTML = '<p>Attempts Remaining: 6/6</p>';
         score = 0;
         attempts = 6;
@@ -56,11 +31,12 @@ const init = () => {
         renderDisplay(displayCards);
     };
 };
+
 const randomizeCards = () => {
-    let cards = [...pairs];
+    let deck = [...pairs];
     for(let i = 0; i < 10; i++) {
-        let randomIdx = Math.floor(Math.random() * cards.length);
-        let draw = cards.splice(randomIdx, 1);
+        let randomIdx = Math.floor(Math.random() * deck.length);
+        let draw = deck.splice(randomIdx, 1);
         displayCards.push(draw[0][0],draw[0][1]);
         draw = [];
         i += 1;
@@ -68,12 +44,11 @@ const randomizeCards = () => {
     displayCards = displayCards.sort(()=> {
         return Math.random() - 0.5;
     });
-    cards = [];
 };
-// 2. Primary Render - Renders randomized cards onto display - Renders Memorization Timer - Game and Phase Start/End Messages.
+
 const renderDisplay = (array) => {
     for(let i = 0; i < 10; i++) {
-    card[i].classList.add(array[i]);
+        card[i].classList.add(array[i]);
     };
     timerFunction(memorization);
     messageUpdate(memorization);
@@ -105,27 +80,25 @@ const timerFunction = (phase) => {
 
 const messageUpdate = (phase) => {
     if(phase === memorization) {
-    messageEl.innerHTML = '';
-    messageEl.innerHTML += '<h2 id="mem-message">You have 10 seconds to memorize!</h2>';
+        messageEl.innerHTML = '<h2 id="mem-message">You have 10 seconds to memorize!</h2>';
     } else if (phase === matching) {
-    messageEl.innerHTML = '';
-    messageEl.innerHTML += '<h2 id="mat-message">You have 30 seconds to match the cards!</h2>';
+        messageEl.innerHTML = '<h2 id="mat-message">You have 30 seconds to match the cards!</h2>';
     };
 };
-// 3. Phase Transition - Triggered by Memorization Timer Elapsing - Starts Matching Timer.
+
 const phaseTransition = () => {
     timerFunction(matching);
     renderTransition(displayCards);
     messageUpdate(matching);
 };
-// 4. Flip Render - Flips over cards when Matching Phase begins - Renders Matching Timer - Reacts to Cards Clicked and Flips/Unflips.
+
 const renderTransition = (array) => {
     for(let i = 0; i < 10; i++) {
         card[i].classList.remove(array[i]);
         card[i].classList.add('back');
     };
 };
-// 5. Handle Click - Reacts to Cards Clicked and Flips/Unflips; rendering them.
+
 const handleClick = (event) => {
     if (state === true) {
         if(event.target.classList.contains('back')) {
@@ -151,14 +124,12 @@ const matchCheck = (first,second) => {
         };
     });
     if(match === 1) {
+        messageEl.innerHTML = '<h2>Correct Match!</h2>';
         updateScore();
-        messageEl.innerHTML = '';
-        messageEl.innerHTML += '<h2>Correct Match!</h2>';
         cardA.pop();
         cardB.pop();
     } else {
-        messageEl.innerHTML = '';
-        messageEl.innerHTML += '<h2>Wrong Match!</h2>';
+        messageEl.innerHTML = '<h2>Wrong Match!</h2>';
         renderUnflip(cardA[0]);
         renderUnflip(cardB[0]);
         cardA.pop();
@@ -171,41 +142,37 @@ const renderFlip = (slot) => {
     slot.classList.remove('back');
     slot.classList.add(displayCards[slot.id]);
 };
+
 const renderUnflip = (slot) => {
     slot.classList.remove(displayCards[slot.id]);
     slot.classList.add('back');
 };
 
-// 6. Score/Attempt Tracking & Render - Updates Score and Attempts as game progresses.
 const updateScore = () => {
     score += 1;
-    scoreEl.innerHTML = '';
-    scoreEl.innerHTML += `<p>Player Score: ${score}/5</p>`;
+    scoreEl.innerHTML = `<p>Player Score: ${score}/5</p>`;
     if(score === 5) {
         return checkOutcome('win');
     };
 };
 const updateAttempts = () => {
     attempts -= 1;
-    attemptEl.innerHTML = '';
     attemptEl.innerHTML = `<p>Attempts Remaining: ${attempts}/6</p>`;
     if(score !== 5 && attempts === 0) {
         return checkOutcome('loss');
     };
 };
-// 7. Game State - Win or Loss [Latter, by time or attempts].
+
 const checkOutcome = (outcome) => {
     if(outcome === 'win') {
-        messageEl.innerHTML = '';
-        messageEl.innerHTML += '<h2>YOU WIN!</h2>';
+        messageEl.innerHTML = '<h2>YOU WIN!</h2>';
     } else if (outcome === 'loss') {
-        messageEl.innerHTML = '';
-        messageEl.innerHTML += '<h2>YOU LOSE!</h2>';
+        messageEl.innerHTML = '<h2>YOU LOSE!</h2>';
     };
     state = false;
     clearRender(displayCards);
 };
-// 8. Reinitialization function - Reset Button clicked - go through Init again.
+
 const clearRender = (array) => {
     for(let i = 0; i < 10; i++) {
         card[i].classList.remove(array[i]);
@@ -213,11 +180,8 @@ const clearRender = (array) => {
     };
 };
 
-
-// Event Listeners
-// 1. The Cards on display (For Each) to be clicked.
 for (i = 0; i < card.length; i++) {
-card[i].addEventListener("click", handleClick);
+    card[i].addEventListener("click", handleClick);
 };
-// 2. Start/Reset Button
+
 button.addEventListener("click", init);
